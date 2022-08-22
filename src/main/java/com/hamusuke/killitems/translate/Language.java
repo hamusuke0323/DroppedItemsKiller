02df51b07f34534d4b6da9840e47ab119417083a
ハムスケ ハム <daiki.esdfth12488@gmail.com>
2022-08-22 21:50:27 +0900

    fix a bug

--- src/main/java/com/hamusuke/killitems/translate/Language.java
+++ src/main/java/com/hamusuke/killitems/translate/Language.java
@@ -1,6 +1,5 @@
 package com.hamusuke.killitems.translate;
 
-import com.google.common.collect.Maps;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.hamusuke.killitems.Main;
@@ -13,17 +12,18 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public final class Language {
     private static final Gson GSON = new Gson();
-    private static final Map<String, Map<String, String>> cached = Maps.newConcurrentMap();
+    private static final Map<String, Map<String, String>> cached = new ConcurrentHashMap<>();
 
     public static String translate(@Nullable Player player, String key, Object... args) {
         return player == null ? translate(Grasscutter.getLanguage().getLanguageCode(), key, args) : translate(Utils.getLanguageCode(player.getAccount().getLocale()), key, args);
     }
 
     private static String translate(String lang, String key, Object... args) {
-        return cached.computeIfAbsent(lang, s -> Maps.newConcurrentMap()).computeIfAbsent(key, s -> {
+        return cached.computeIfAbsent(lang, s -> new ConcurrentHashMap<>()).computeIfAbsent(key, s -> {
             InputStream is = Main.getInstance().getClass().getClassLoader().getResourceAsStream(lang + ".json");
             if (is == null) {
                 Grasscutter.getLogger().warn("Language Json file, {} not found!", lang + ".json");
